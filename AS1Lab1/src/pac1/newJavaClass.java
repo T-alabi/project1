638d3dd2738da484d2e46ae08bf8b1f1b5956bab
tmala <tmala@macbook-air-2.fios-router.home>
2022-01-24 20:22:58 -0500

    Initial project commit

---
+++ AS1Lab1/src/pac1/newJavaClass.java
@@ -0,0 +1,8 @@
+package pac1;
+
+public class newJavaClass {
+	public static void main(String[] args) {
+		System.out.println("I just installed the newest java and changed the eclipse. ini file!");
+	}
+
+}
